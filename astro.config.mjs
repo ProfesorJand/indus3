d283475b99adb2d28f7be6a94b635bb193721aa2
profesorjand <profesorjand@gmail.com>
2026-04-19 18:46:45 -0300

    cambios en los urls de base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://astro.build/config
 export default defineConfig({
   site: 'https://indus3pro.com', // Cambia 'ProfesorJand' por tu usuario si es diferente
+   base: '/',
 
   integrations: [react()],
   vite: {

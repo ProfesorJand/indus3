70fec731011676d08ee7e4653f3650080e9b7a3e
profesorjand <profesorjand@gmail.com>
2026-04-28 12:10:17 -0300

    arreglo de eventos para que tome datos del backend

---
+++ src/stores/eventos.js
@@ -0,0 +1,12 @@
+export const urlEventos = "http://api.indus3pro.com/eventos/get-eventos.php?t="+Date.now();
+
+export const getEventos = async () => {
+  const response = await fetch(urlEventos, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${import.meta.env.PUBLIC_BACKEND_AUTH_KEY}`
+    },
+  });
+  const data = await response.json();
+  return data.data;
+}
